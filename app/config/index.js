995847c762b61dfb9d2ecb60e70098064dfd08b9
Nhi13012003 <mjnhnhi333@gmail.com>
2025-07-25 19:02:52 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -2,6 +2,9 @@ const config = {
     app: {
         port: process.env.PORT || 3000,
     },
+    db: {
+        uri: process.env.DB_URI || 'mongodb://localhost:27017/contactbook',
+    }
 }
 
 module.exports = config;
